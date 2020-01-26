3886b5bbecefc704f3c347228180485b198d28e6
curious_odd_man <vlad.varslavans@gmail.com>
2020-01-26 21:32:27 +0200

    Fix for number format error

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -9,6 +9,7 @@ import scala.io.Source
 object main {
 
   private val FILE_TO_STORE_VIEWED_ADS = "AlreadyViewedAds.log"
+  private val FILE_TO_STORE_LINKS_TO_ADS = "AlreadyViewedAdsLinks.log"
   private val OUTPUT_HTML_FILE = "NewAds.html"
 
   def toInt(s: String): Int = {
@@ -19,8 +20,9 @@ object main {
     }
   }
 
-  case class Advertisement(text: String, rooms: Integer, area: Float, address: String, seria: String, floor: String, price: Float, pricePerMeter: Float, homeType: String, originalLink: String) {
+  case class Address(city: String, district: String, street: String)
 
+  case class Advertisement(text: String, rooms: Integer, area: Float, seria: String, floor: String, price: Float, pricePerMeter: Float, homeType: String, originalLink: String, address: Address) {
     def roomsBetween(min: Int, max: Int): Boolean = {
       min <= rooms && rooms <= max
     }
@@ -50,9 +52,10 @@ object main {
     val indexOfNumberStart = lastPageLinkeString.indexOf("/page")
     val lastPageIndex = lastPageLinkeString.substring(indexOfNumberStart + 5, lastPageLinkeString.length - ".html".length).toInt
 
+    @scala.annotation.tailrec
     def mainloop(index: Int, docs: List[Document]): List[Document] = {
-      val link = "https://m.ss.com/ru/real-estate/flats/riga/all/hand_over/page" + index + ".html"
-      Thread.sleep((Math.random() * 1000).toLong)   // avoid spamming ss.com with requests
+      val link = "https://m.ss.com/ru/real-estate/flats/riga/all/sell/page" + index + ".html"
+      Thread.sleep((Math.random() * 1000).toLong) // avoid spamming ss.com with requests
       print(".")
       if (index > lastPageIndex) {
         println(" DONE!")
@@ -66,11 +69,14 @@ object main {
     mainloop(2, List(firstPage))
   }
 
-  private def linkToAdvertisement(link: String): Advertisement = {
+  private def numToArea(num: String): Float = {
+    num.replaceAll(" м²", "").toFloat
+  }
 
+  private def linkToAdvertisement(link: String): Advertisement = {
     print(".")
     val doc = Jsoup.connect(link).get()
-    Thread.sleep((Math.random() * 1000).toLong)   // avoid spamming ss.com with requests
+    Thread.sleep((Math.random() * 1000).toLong) // avoid spamming ss.com with requests
     val main = doc.getElementById("main")
     val table = main.getElementsByTag("tr").toList
 
@@ -87,7 +93,10 @@ object main {
 
     val prices = map("Цена:").replace(" ", "").replace("(", "").split("€")
 
-    Advertisement(main.text(), toInt(map("Комнат:")), map("Площадь:").toFloat, map("Улица:"), map("Серия:"), map("Этаж / этажей:"), prices(0).toFloat, 0, map("Тип дома:"), link)
+    val addr = Address(map("Город:"), map("Район:"), map("Улица:"))
+    val ad = Advertisement(main.text(), toInt(map("Комнат:")), numToArea(map("Площадь:")), map("Серия:"), map("Этаж / этажей:"), prices(0).toFloat, 0, map("Тип дома:"), link, addr)
+    println(ad)
+    ad
   }
 
   def main(args: Array[String]): Unit = {
@@ -111,17 +120,34 @@ object main {
     if (!Files.exists(path))
       Files.createFile(path)
 
-    val viewedAds = Source.fromFile(FILE_TO_STORE_VIEWED_ADS).getLines().map(_.toInt).toSet
+    val path1 = Paths.get(FILE_TO_STORE_LINKS_TO_ADS)
+
+    if (!Files.exists(path1))
+      Files.createFile(path1)
+
+    val viewedAdsSource = Source.fromFile(FILE_TO_STORE_VIEWED_ADS)
+    val linksOfViewedAdsSource = Source.fromFile(FILE_TO_STORE_LINKS_TO_ADS)
+
+    val viewedAds = viewedAdsSource.getLines().map(_.toInt).toSet
+    viewedAdsSource.close()
+    val linksOfViewedAds = linksOfViewedAdsSource.getLines().toSet
+    linksOfViewedAdsSource.close()
 
     println("Already seen " + viewedAds.size + " ads.")
 
-    val allPages = getAllPages("https://m.ss.com/ru/real-estate/flats/riga/all/hand_over/")
+    val allPages = getAllPages("https://m.ss.com/ru/real-estate/flats/riga/all/sell/")
 
     println("Found " + allPages.size + " new pages.")
 
     print("Extracting ads information: ")
-    val allNewAds = allPages.flatMap(extractLinks).map(linkToAdvertisement).filter(_.roomsBetween(minRooms, maxRooms)).filterNot(adv => viewedAds.contains(adv.hashCode()))
-    println( "DONE!")
+    val allNewAds = allPages.par.flatMap(extractLinks)
+      .filterNot(lnk => linksOfViewedAds.contains(lnk))
+      .map(linkToAdvertisement)
+      .filter(_.roomsBetween(minRooms, maxRooms))
+      .filterNot(adv => viewedAds.contains(adv.hashCode()))
+      .filter(ad => Seq("центр", "дарзциемс", "кенгарагс", "плявниеки", "пурвциемс", "катлакалнс", "зиепниеккалнс", "торнякалнс").contains(ad.address.district.toLowerCase))
+
+    println("DONE!")
 
     println("Found " + allNewAds.size + " new ads.")
 
