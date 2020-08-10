6055d512541f29ce474595459875a92ac80fb537
curious_odd_man <vlad.varslavans@gmail.com>
2020-08-10 22:26:35 +0300

    Added spark

--- build.sbt
+++ build.sbt
@@ -2,10 +2,15 @@ name := "FindMeApartment"
 
 version := "0.1"
 
-scalaVersion := "2.10.6"
+scalaVersion := "2.11.12"
 
 // https://mvnrepository.com/artifact/org.jsoup/jsoup
 libraryDependencies += "org.jsoup" % "jsoup" % "1.10.3"
+// https://mvnrepository.com/artifact/org.apache.spark/spark-core
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.4.6"
+// https://mvnrepository.com/artifact/org.apache.spark/spark-sql
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.4.6"
+
 
 
         
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,8 @@
 import java.nio.file.{Files, Paths, StandardOpenOption}
 import java.util.Objects
 
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
 import org.jsoup.Jsoup
 import org.jsoup.nodes.{Document, Element}
 
@@ -8,13 +10,16 @@ import scala.collection.JavaConversions._
 import scala.collection.immutable.HashSet
 import scala.io.Source
 
-object main {
+object Main {
 
-  private val FILE_TO_STORE_KNOWN_ADS = "AllLinks.log"
-  private val OUTPUT = "OUTPUT.log"
+  private val FILE_TO_STORE_KNOWN_ADS = "AllLinks_119.log"
+  private val OUTPUT = "OUTPUT_119.log"
   private val FILE_TO_STORE_VIEWED_ADS = "AlreadyViewedAds.log"
   private val FILE_TO_STORE_LINKS_TO_ADS = "AlreadyViewedAdsLinks.log"
   private val OUTPUT_HTML_FILE = "NewAds.html"
+
+  private val LINK_HAND_OVER = "https://m.ss.com/ru/real-estate/flats/riga/all/hand_over/"
+  private val LINK_SELL = "https://m.ss.com/ru/real-estate/flats/riga/all/sell/"
 
   def toInt(s: String): Int = {
     try {
@@ -58,7 +63,7 @@ object main {
 
     @scala.annotation.tailrec
     def mainloop(index: Int, docs: List[Document]): List[Document] = {
-      val link = "https://m.ss.com/ru/real-estate/flats/riga/all/sell/page" + index + ".html"
+      val l = link + "page" + index + ".html"
       Thread.sleep((Math.random() * 1000).toLong) // avoid spamming ss.com with requests
       print(".")
       if (index > lastPageIndex) {
@@ -66,7 +71,7 @@ object main {
         docs
       }
       else {
-        mainloop(index + 1, Jsoup.connect(link).get() :: docs)
+        mainloop(index + 1, Jsoup.connect(l).get() :: docs)
       }
     }
 
@@ -104,44 +109,64 @@ object main {
     ad
   }
 
-  def main(args: Array[String]): Unit = {
-    val knownAds = Paths.get(FILE_TO_STORE_KNOWN_ADS)
+  def handleAll(fileToStoreKnownAds: String, output: String, link: String): Unit = {
+    val knownAds = Paths.get(fileToStoreKnownAds)
     if (!Files.exists(knownAds))
       Files.createFile(knownAds)
 
-    val outputPath = Paths.get(OUTPUT)
+    val outputPath = Paths.get(output)
     if (!Files.exists(outputPath))
       Files.createFile(outputPath)
 
-    val allPages = getAllPages("https://m.ss.com/ru/real-estate/flats/riga/all/sell/")
+    val allPages = getAllPages(link)
     println("Found " + allPages.size + " new pages.")
 
-    val viewedAdsSource = Source.fromFile(FILE_TO_STORE_KNOWN_ADS)
+    val viewedAdsSource = Source.fromFile(fileToStoreKnownAds)
     val viewedAds = HashSet() ++ viewedAdsSource.getLines()
     viewedAdsSource.close()
 
     allPages.toStream
       .flatMap(extractLinks)
       .map(linkToAdvertisement)
-      .filter(ad => ad.address.district.toLowerCase.equals("кенгарагс"))
-      .filter(ad => ad.roomsBetween(3, 4))
-      .filter(ad => ad.seria == "Лит. пр.")
+      //.filter(ad => ad.address.district.toLowerCase.equals("кенгарагс"))
+      .filter(ad => ad.roomsBetween(2, 3))
+      .filter(ad => ad.seria == "119-я" || ad.seria == "103-я" || ad.seria == "Нов.")
       .map(v => {
         println(v)
         v
       })
       .filter(ad => {
-      val res = !viewedAds.contains(ad.toString)
-      println("After filter: " + res)
-      res
-    })
+        val res = !viewedAds.contains(ad.toString)
+        println("After filter: " + res)
+        res
+      })
       .map(ad => {
         Files.write(knownAds, (ad.toString + "\n").getBytes, StandardOpenOption.APPEND)
         ad
       })
-      .map(ad => List(ad.address.street, ad.floor, ad.rooms, ad.price, ad.originalLink).mkString("\t"))
-      .foreach(str => Files.write(outputPath, (str + "\n").getBytes, StandardOpenOption.APPEND))
+      .map(ad => List(ad.seria, ad.address.street, ad.floor, ad.rooms, ad.price, ad.originalLink).mkString("\t"))
+      .foreach(str => Files.write(outputPath, (str + "\t" + java.time.LocalDate.now + "\n").getBytes, StandardOpenOption.APPEND))
+  }
 
+  def linesToSpark(sc: SparkContext, file: String): RDD[(String, String)] = {
+    sc.textFile(file).map(l => l.split('\t')).map(arr => (s"${arr(0)}_${arr(1)}_${arr(2)}_${arr(3)}", arr(5))).reduceByKey((_, k2) => k2)
+  }
+
+  def findProbableOverlap(sell: String, handOver: String): Unit = {
+    val conf = new SparkConf().setAppName("appName").setMaster("local[*]")
+    val sc = new SparkContext(conf)
+    val groupingSell = linesToSpark(sc, sell)
+    val groupingHandOver = linesToSpark(sc, handOver)
+    val joined = groupingSell.join(groupingHandOver).collect()
+
+    joined.foreach(str => Files.write(Paths.get("ProbableSellAndHandOver"), (str + "\t" + java.time.LocalDate.now + "\n").getBytes, StandardOpenOption.APPEND, StandardOpenOption.CREATE))
+  }
+
+  def main(args: Array[String]): Unit = {
+    handleAll(FILE_TO_STORE_KNOWN_ADS, OUTPUT, LINK_SELL)
+    handleAll(FILE_TO_STORE_KNOWN_ADS + "_hand_over", OUTPUT + "_hand_over", LINK_HAND_OVER)
+
+    findProbableOverlap(OUTPUT, OUTPUT + "_hand_over")
 
     //    val linksOfViewedAdsSource = Source.fromFile(FILE_TO_STORE_LINKS_TO_ADS)
     //    val linksOfViewedAds = linksOfViewedAdsSource.getLines().toSet
